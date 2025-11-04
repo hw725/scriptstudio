1fe2612d857d718f2e9c7bef24f84601242c48c3
hw725 <hw725@g.skku.edu>
2025-11-04 17:12:16 +0900

    fix: IndexedDB 초기화 추가 (main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App.jsx";
+import "@/index.css";
+import { initDB } from "@/db/localDB";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-) 
+// IndexedDB 초기화
+initDB().catch(console.error);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
