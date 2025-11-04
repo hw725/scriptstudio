efcc690182053a875f1b5d336a1b0f834c817f81
hw725 <hw725@g.skku.edu>
2025-11-04 16:30:25 +0900

    feat: Google OAuth - AuthProvider 활성화

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Pages from "@/pages/index.jsx";
 import { Toaster } from "@/components/ui/toaster";
-// import { Base44AuthProvider } from "@/components/providers/Base44AuthProvider"
+import { Base44AuthProvider } from "@/components/providers/Base44AuthProvider";
 
 function App() {
   return (
-    <>
+    <Base44AuthProvider>
       <Pages />
       <Toaster />
-    </>
+    </Base44AuthProvider>
   );
 }
 
